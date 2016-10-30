ce440d8506403cbb7b8ed73dc6e5e0c9bb137efd
tonyakwei <tonyakwei@gmail.com>
2016-10-30 07:08:08 -0400

    Update step4.py

--- step4.py
+++ step4.py
@@ -1,22 +1,28 @@
 import json
 import requests
 
+
 HTTPHeaders = {'content-type': 'application/json'}
 tokenDict = {'token': '5185bcac830273a1051d4c260585f734'}
-challengeURL = 'http://challenge.code2040.org/api/haystack'
-validationURL = 'http://challenge.code2040.org/api/haystack/validate'
+challengeURL = 'http://challenge.code2040.org/api/prefix'
+validationURL = 'http://challenge.code2040.org/api/prefix/validate'
 
-needleHaystackDict = json.loads(requests.post(challengeURL, data=json.dumps(tokenDict), headers=HTTPHeaders).text)
+prefixDict = json.loads(requests.post(challengeURL, data=json.dumps(tokenDict), headers=HTTPHeaders).text)
+prefix = prefixDict['prefix']
+prefixLength = len(prefix)
 
-needle = needleHaystackDict['needle']
-index = 0;
+words = []
 
-for i in range(0, len(needleHaystackDict['haystack'])-1):
-    if needle == needleHaystackDict['haystack'][i]:
-        index = i
-        break
+for i in range(0, len(prefixDict['array'])-1):
+    if(prefixDict['array'][i][0:prefixLength] != prefix):
+        words.append(unicode(prefixDict['array'][i]))
 
-answerDict = {'token': '5185bcac830273a1051d4c260585f734', 'needle': index}
+print prefixDict['prefix']
+print prefixDict['array']
+print words
+
+answerDict = {'token': '5185bcac830273a1051d4c260585f734', 'array': words}
+print answerDict
 
 response = requests.post(validationURL, data=json.dumps(answerDict), headers=HTTPHeaders)
 
